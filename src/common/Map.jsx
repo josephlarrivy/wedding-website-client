f66cecf8f4ea7e0220dd21656e1647f8d4261dc3
josephlarrivy <joseph.larrivy@gmail.com>
2023-12-20 16:58:34 -0600

    fixed icon marker

--- src/common/Map.jsx
+++ src/common/Map.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet'; // Import Leaflet library
 import 'leaflet/dist/leaflet.css';
 
+// Import your custom marker icon
+import customMarkerIcon from '../images/blue-square.png';
+
 const Map = () => {
+  // Create a custom icon instance
+  const customIcon = new L.Icon({
+    iconUrl: customMarkerIcon, // Path to your custom icon
+    iconSize: [32, 32], // Size of the icon
+    iconAnchor: [16, 32], // Anchor point of the icon (centered, bottom)
+    popupAnchor: [0, -32], // Anchor point for popups (top, centered)
+  });
+
   return (
     <MapContainer
-          center={[46.600420704108735, -91.57172569008635]}
+      center={[46.600420704108735, -91.57172569008635]}
       zoom={8}
       style={{ width: '100%', height: '400px', zIndex: '0' }}
     >
@@ -13,13 +25,16 @@ const Map = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-          <Marker position={[46.600420704108735, -91.57172569008635]}>
+      {/* Use the Marker component with the custom icon */}
+      <Marker position={[46.600420704108735, -91.57172569008635]} icon={customIcon}>
         <Popup>
-          <p><b>Brule River Barn</b>
-          <br></br>
-          4492 S County Highway H
-          <br></br>
-          Brule, WI 54820</p>
+          <p>
+            <b>Brule River Barn</b>
+            <br></br>
+            4492 S County Highway H
+            <br></br>
+            Brule, WI 54820
+          </p>
         </Popup>
       </Marker>
     </MapContainer>
